08dab2803610053d200520f41b7c95c4570fabee
Yu-7777 <usagitokame777@outlook.jp>
2025-05-25 05:44:34 +0000

    add group_member relationship

--- app/Models/GroupMember.php
+++ app/Models/GroupMember.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Group;
+use App\Models\User;
+
 class GroupMember extends Model
 {
     use HasFactory;
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
