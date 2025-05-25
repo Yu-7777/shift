ca757dadd52fc009e91816e148b6d59978cee5bc
Yu-7777 <usagitokame777@outlook.jp>
2025-05-25 06:05:42 +0000

    remove unnecessary use

--- app/Models/GroupMember.php
+++ app/Models/GroupMember.php
@@ -5,9 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-use App\Models\Group;
-use App\Models\User;
-
 class GroupMember extends Model
 {
     use HasFactory;
