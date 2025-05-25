7147c0390d1940263a0793bb13309d72fa4c0fb3
Yu-7777 <usagitokame777@outlook.jp>
2025-05-25 15:50:45 +0000

    add comment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,6 +18,7 @@ class UserController extends Controller
         return view('users.show', compact('user'));
     }
 
+    // ユーザーが所属しているバイトグループの一覧を取得
     public function groups(User $user)
     {
         $groups = $user->groups;
