70cb8884e3043d75a0a75a3561e01a5b4c2e6d58
Yu-7777 <usagitokame777@outlook.jp>
2025-05-25 08:35:59 +0000

    add users index & show

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('users.index');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show')->with(['user' => $user]);
+    }
 }
