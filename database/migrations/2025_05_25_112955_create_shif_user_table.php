9c660fabefda0e39410b75352dff33faa98ffacf
Yu-7777 <usagitokame777@outlook.jp>
2025-05-25 05:18:18 +0000

    fix-typo

--- database/migrations/2025_05_25_112955_create_shif_user_table.php
+++ database/migrations/2025_05_25_112955_create_shif_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('shif_user', function (Blueprint $table) {
+        Schema::create('shift_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->foreignId('shift_id')->constrained();
