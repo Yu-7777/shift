cf6a1fec85bfc3bbab68f7471687f70af2a686b3
Yu-7777 <usagitokame777@outlook.jp>
2025-06-23 23:33:56 +0900

    UserTestの追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,7 +46,7 @@ class User extends Authenticatable
     public function getShiftGroups(int $limit_count = 10)
     {
         return $this->belongsToMany(Group::class, 'group_members')
-                    ->withCount('group_members')
+                    ->withCount('group_members') // プロパティにメンバーの人数を追加
                     ->orderBy('group_members.created_at', 'desc')
                     ->limit($limit_count)
                     ->get();
---
+++ database/factories/GroupMemberFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Group;
+use App\Models\Role;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\GroupMember>
+ */
+class GroupMemberFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'group_id' => Group::factory(),
+            'role_id' => Role::factory(),
+        ];
+    }
+
+    public function withUser(User $user): Factory
+    {
+        return $this->state(function (array $attributes) use ($user) {
+            return [
+                'user_id' => $user->id,
+            ];
+        });
+    }
+
+    public function withGroup(Group $group): Factory
+    {
+        return $this->state(function (array $attributes) use ($group) {
+            return [
+                'group_id' => $group->id,
+            ];
+        });
+    }
+}
